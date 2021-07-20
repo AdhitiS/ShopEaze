a8a0b39678ac066a3186fe5bcb3824f52ca3a4e3
Adhiti <shaili.adhiti@gmail.com>
2021-07-20 21:02:42 +0530

    History

--- server/routes/user.js
+++ server/routes/user.js
@@ -11,12 +11,17 @@ const {
   emptyCart,
   saveAddress,
   applyCouponToUserCart,
+  createOrder,
+  orders,
 } = require("../controllers/user");
 
 router.post("/user/cart", authCheck, userCart); // save cart
 router.get("/user/cart", authCheck, getUserCart); // get cart
 router.delete("/user/cart", authCheck, emptyCart); // empty cart
 router.post("/user/address", authCheck, saveAddress);
+
+router.post("/user/order", authCheck, createOrder);
+router.get("/user/orders", authCheck, orders);
 
 // coupon
 router.post("/user/cart/coupon", authCheck, applyCouponToUserCart);
@@ -28,4 +33,3 @@ router.post("/user/cart/coupon", authCheck, applyCouponToUserCart);
 // });
 
 module.exports = router;
-
